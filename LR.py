08c6f79f43cc2b5c5c6bbde35a092a05c5c2744c
lizhongguo <lizhongguo@enn.cn>
2021-07-29 18:22:49 +0800

    临时更改

--- LR.py
+++ LR.py
@@ -20,12 +20,12 @@ class VT:
     def __eq__(self, o: object) -> bool:
         if not o:
             return False
-        if not isinstance(o, self.__class__):
+        if not isinstance(o, VT):
             return False
         return self.tag == o.tag
 
     def __hash__(self) -> int:
-        return hash(self.tag) ^ hash(self.value)
+        return hash(self.tag)
 
     def __str__(self) -> str:
         return 'VT(tag: %s, value: %s)' % (self.tag, str(self.value)) 
@@ -39,7 +39,7 @@ class VN:
     def __eq__(self, o: object) -> bool:
         if not o:
             return False
-        if not isinstance(o, self.__class__):
+        if not isinstance(o, VN):
             return False
         return self.tag == o.tag
 
@@ -65,20 +65,129 @@ class Rule:
 
         self.children.add(tuple(child))
 
-class Item:
+class SingleRule:
+    """ 单独拆分出来的一条推导规则
+    """    
+    def __init__(self, parent:VN, child) -> None:
+        self.parent = parent
+        self.child:tuple = tuple(child)
+
+    def __hash__(self) -> int:
+        return hash(self.parent) ^ hash(self.child)
+
+    def __eq__(self, o: object) -> bool:
+        if o is None:
+            return False
+        
+        if not isinstance(o, SingleRule):
+            return False
+        
+        return self.parent == o.parent and self.child == o.child
+
+class ProjectItem:
     # 项目, 某个规则的前pos个条件已经满足
-    def __init__(self) -> None:
-        self.rule = None
+    # 每一个项目由项目id, 和项目位置唯一标定
+    def __init__(self, rule:SingleRule, pos:int) -> None:
+        self.rule = rule
         self.pos = 0
 
-        # 展望符
-        self.expectedVT = None
+        # 空推导式直接进入规约状态
+        if self.pos<len(self.rule) and self.rule[self.pos] == EPSILON:
+            self.pos = 1
 
-class ItemsSet:
+    def nextToken(self):
+        if self.pos >= len(self.rule.child):
+            return None
+        return self.rule.child[self.pos]
+
+    def restToken(self):
+        return self.rule.child[self.pos]
+
+    def __hash__(self) -> int:
+        return hash(self.rule) ^ hash(self.pos)
+
+    def __eq__(self, o: object) -> bool:
+        if o is None:
+            return False
+        if isinstance(o, self.__class__):
+            return self.rule == o.rule and self.pos == o.pos
+        return False
+
+class ProjectItemExtends:
+    # 项目, 某个规则的前pos个条件已经满足
+    # 每一个项目由项目id, 和项目位置, 和展望符唯一标定
+    def __init__(self, rule:SingleRule, pos:int, expectedVT:set[VT]) -> None:
+        self.projectItem = ProjectItem(rule, pos)
+
+        # 展望符
+        self.expectedVT = set( vt for vt in expectedVT)
+
+    def nextToken(self):
+        return self.projectItem.nextToken()
+
+    def __hash__(self) -> int:
+        v = 0
+        for vt in self.expectedVT:
+            v ^= hash(vt)
+        return hash(self.projectItem) ^ v
+
+    def __eq__(self, o: object) -> bool:
+        if o is None:
+            return False
+        if isinstance(o, self.__class__):
+            return self.projectItem == o.projectItem and self.expectedVT == o.expectedVT
+        return False
+
+class Project:
     # 项目集
     def __init__(self) -> None:        
-        pass
- 
+        self.items = set()
+
+    def addItem(self, item:ProjectItemExtends):
+        if isinstance(item, set):
+            self.items |= item
+            return
+        self.items.add(item)
+
+    def __hash__(self) -> int:
+        v = 0
+        for item in self.items:
+            v ^= hash(item)
+        return v
+
+    def __eq__(self, o: object) -> bool:
+        if o is None:
+            return False
+        if isinstance(o, self.__class__):
+            return self.items == o.items
+        return False
+
+class ProjectExtends:
+    # 项目集
+    # 项目 -> 展望符
+    def __init__(self) -> None:        
+        self.items = set()
+
+    def addItem(self, item:ProjectItemExtends):
+        if isinstance(item, set):
+            self.items |= item
+            return
+        self.items.add(item)
+
+    def __hash__(self) -> int:
+        v = 0
+        for item in self.items:
+            v ^= hash(item)
+        return v
+
+    def __eq__(self, o: object) -> bool:
+        if o is None:
+            return False
+        if isinstance(o, self.__class__):
+            return self.items == o.items
+        return False
+
+
 class NFA:
     def __init__(self) -> None:
         pass
@@ -184,7 +293,7 @@ def testConstructFirstSet():
     printSet(First)
 
 # 获取某个序列的First集合
-def getFirstOfSeq(First:dict[VN, Rule], seq:Iterable):
+def getFirstOfSeq(First:dict[VN, VT], seq:Iterable):
     results = set()
 
     if len(seq) == 0:
@@ -209,7 +318,7 @@ def getFirstOfSeq(First:dict[VN, Rule], seq:Iterable):
     return results
 
 # 构造Follow集
-def constructFollowSet(rules:dict[VN,Rule], First:dict[VN, Rule], beginning:VN):
+def constructFollowSet(rules:dict[VN,Rule], First:dict[VN, VT], beginning:VN):
     # 首先在开始规则的Follow集放入 $
     # 遍历所有规则, 更新Follow集合,直到Follow集合不再更新
     updated = True
@@ -237,16 +346,66 @@ def constructFollowSet(rules:dict[VN,Rule], First:dict[VN, Rule], beginning:VN):
                     if isinstance(child[i], VN):
                         updated = True if updateSet(Follow[child[i]], getFirstOfSeq(First, child[i+1:]), True) else updated
 
-                #如果b->epsilon, A与B的Follow集相同
+                #如果b->epsilon, B的Follow集增加Follow(A)
                 for i in range(len(child)-1, 0, -1):
                     if isinstance(child[i], VN):
                         if EPSILON in getFirstOfSeq(First,child[i+1:]):
-                            us = Follow[parentVN] | Follow[child[i]]
-                            updated = True if updateSet(Follow[parentVN], us, True) else updated
-                            updated = True if updateSet(Follow[child[i]], us, True) else updated
+                            updated = True if updateSet(Follow[child[i]], Follow[parentVN], True) else updated
 
 
     return Follow
+
+class Action:
+    def __init__(self) -> None:
+        pass
+
+class LR_FSA:
+    def __init__(self) -> None:
+        pass
+
+# 计算某一项目的派生
+def deriveItem(rules:dict[VN, Rule], First:dict[VN, VT], curItem:ProjectItemExtends):
+    nextToken =  curItem.nextToken()
+    expectedVT = set(curItem.expectedVT)
+
+    nextItems = set()
+    if not nextToken:
+        # 规约项目没有后继
+        return nextItems
+
+    # 接收nextToken, 状态转移
+    coreToExpectedVT:dict[ProjectItem, set[VT]] = dict()
+    coreToExpectedVT[ProjectItem(curItem.projectItem.rule, curItem.projectItem.pos+1)] = expectedVT
+
+    # nextItems.add(ProjectItemExtends(curItem.rule, curItem.pos+1, curItem.expectedVT))
+    
+    # 计算项目闭包
+    updated = True
+    while updated:
+        updated = False
+        for projectItem in coreToExpectedVT:
+            if isinstance(projectItem.nextToken(), VN):
+                rule = rules[projectItem.nextToken()]
+                for child in rule.children:
+                    followSet = getFirstOfSeq(First, projectItem.restToken() + coreToExpectedVT[projectItem])
+                    derivedProjectItem = ProjectItem(SingleRule(rule.parent,child), 0)
+
+                    if derivedProjectItem not in coreToExpectedVT:
+                        coreToExpectedVT[derivedProjectItem] = set()
+                        updated = True
+                    
+                    if not followSet.issubset(coreToExpectedVT[derivedProjectItem]):
+                        coreToExpectedVT[derivedProjectItem] |= followSet
+                        updated = True
+    
+    # 返回项目集合
+
+# 构建LR(1)自动机
+def constructLR1(rules:dict[VN, Rule], beginning:VN):
+    # 自动机定义
+    # 项目集定义
+    # 动作状态
+    pass
 
 def testConstructFollowSet():
     E, E_, T, T_, F, PLUS, MUL, LB, RB, ID = VN('E'), VN('E_'), VN('T'), VN('T_'), VN('F'), VT('PLUS','+'), VT('MUL','*'), VT('LB','('), VT('RB', ')'), VT('ID', 'id')
@@ -273,11 +432,11 @@ def testConstructFollowSet():
     rules = {E:rule_E, E_:rule_E_, T:rule_T, T_:rule_T_, F:rule_F}
     
     First = constructFirstSet(rules)
-    printSet(First)
+    # printSet(First)
 
     Follow = constructFollowSet(rules, First, E)
 
-    # printFirst(First)
+    # printSet(First)
     printSet(Follow)
 
 # LR(1)
@@ -298,4 +457,13 @@ def parseExpression():
     pass
 
 if __name__ == '__main__':
-    testConstructFollowSet()
+    # testConstructFollowSet()
+    E, E_, T, T_, F, PLUS, MUL, LB, RB, ID = VN('E'), VN('E_'), VN('T'), VN('T_'), VN('F'), VT('PLUS','+'), VT('MUL','*'), VT('LB','('), VT('RB', ')'), VT('ID', 'id')
+
+    rule_A = SingleRule(E, (T, E_))
+    rule_B = SingleRule(E, (T, E_, F))
+    rule_C = SingleRule(E, (T, F, E_))
+    rule_D = SingleRule(E, (T, E_, F))
+
+    print(hash(rule_A), hash(rule_B), hash(rule_C), hash(rule_D))
+    print(rule_A == rule_B, rule_B == rule_C, rule_B == rule_D)
